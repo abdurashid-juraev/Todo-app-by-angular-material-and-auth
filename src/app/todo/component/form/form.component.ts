1da1580c825ab7fe6fa6235c3a10b1141d683a3b
Abdurashid <aj6243434@gmail.com>
2024-11-19 22:59:11 +0500

    input and buttons

--- src/app/todo/component/form/form.component.ts
+++ src/app/todo/component/form/form.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
