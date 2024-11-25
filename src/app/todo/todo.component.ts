e4313464169d496f9ebcaa1672ca67c74e849b38
Abdurashid <aj6243434@gmail.com>
2024-11-25 07:26:14 +0500

    fix btn

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-import { FormComponent } from './component/form/form.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [FormComponent],
-  templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss',
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+  ],
+  templateUrl: 'todo.component.html',
+  styleUrl: 'todo.component.scss',
 })
-export class TodoComponent {}
+export class TodoComponent {
+  todo: string = '';
+
+  onSubmit(): void {}
+}
