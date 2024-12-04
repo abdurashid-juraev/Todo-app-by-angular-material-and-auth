77b268fbdced8dab8d73af858aed0ecdbac3e969
Abdurashid <aj6243434@gmail.com>
2024-12-05 01:01:28 +0500

    signin page and validations finish

--- src/app/pages/reg/reg.component.ts
+++ src/app/pages/reg/reg.component.ts
@@ -1,18 +1,18 @@
 import { Component, signal } from '@angular/core';
 import {
   FormControl,
-  FormGroupDirective,
-  NgForm,
-  Validators,
+  FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+  AbstractControl,
 } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-reg',
   standalone: true,
@@ -25,13 +25,74 @@ import { MatIconModule } from '@angular/material/icon';
     ReactiveFormsModule,
     MatButtonModule,
     MatIconModule,
+    CommonModule,
   ],
 })
 export class RegComponent {
   hide = signal(true);
+  // formGroup!: FormGroup;
   clickEvent(event: MouseEvent): void {
     this.hide.set(!this.hide());
     event.stopPropagation();
   }
-  usernameFormControl = new FormControl('');
+  regForm = new FormGroup(
+    {
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        this.containUppercase,
+        this.containsLowercase,
+        this.containsNumber,
+      ]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    },
+    { validators: this.passwordsMatch }
+  );
+
+  get emailControl() {
+    return this.regForm.get('email');
+  }
+
+  get passwordControl() {
+    return this.regForm.get('password');
+  }
+  get confirmPasswordControl() {
+    return this.regForm.get('confirmPassword');
+  }
+  passwordsMatch(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value.trim();
+    const confirmPassword = control.get('confirmPassword')?.value.trim();
+    return password && confirmPassword && password === confirmPassword
+      ? null
+      : { passwordMismatch: true };
+  }
+
+  containUppercase(control: AbstractControl): ValidationErrors | null {
+    const hasUppercase = /[A-Z]/.test(control.value || '');
+    return hasUppercase ? null : { noUppercase: true };
+  }
+  containsLowercase(control: AbstractControl): ValidationErrors | null {
+    const hasLowercase = /[a-z]/.test(control.value || '');
+    return hasLowercase ? null : { noLowercase: true };
+  }
+  containsNumber(control: AbstractControl): ValidationErrors | null {
+    const hasNumber = /[0-9]/.test(control.value || '');
+    return hasNumber ? null : { noNumber: true };
+  }
+  onSubmit(): void {
+    const usernameControl = this.regForm.get('username');
+    const emailControl = this.regForm.get('email');
+
+    if (usernameControl && usernameControl.value) {
+      usernameControl.setValue(usernameControl.value.trim());
+    }
+
+    if (emailControl && emailControl.value) {
+      emailControl.setValue(emailControl.value.trim());
+    }
+    console.warn(this.regForm);
+    console.warn(this.regForm.errors);
+  }
 }
