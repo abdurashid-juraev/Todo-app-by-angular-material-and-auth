81a37bfe908dc184ec4b2db623c9467adfbed37b
Abdurashid <aj6243434@gmail.com>
2024-12-13 00:48:42 +0500

    fix password validations

--- src/app/pages/reg/reg.component.ts
+++ src/app/pages/reg/reg.component.ts
@@ -81,17 +81,16 @@ export class RegComponent {
   }
   passwordValidators(control: AbstractControl): ValidationErrors | null {
     const value = control.value || '';
+    if (!value) return null;
     const errors: ValidationErrors = {};
 
     if (!/[A-Z]/.test(value)) {
       errors['containUppercase'] =
         'Password must contain at least one uppercase letter.';
-    }
-    if (!/[a-z]/.test(value)) {
+    } else if (!/[a-z]/.test(value)) {
       errors['containLowercase'] =
         'Password must contain at least one lowercase letter.';
-    }
-    if (!/[0-9]/.test(value)) {
+    } else if (!/[0-9]/.test(value)) {
       errors['containNumber'] = 'Password must contain at least one number.';
     }
     return Object.keys(errors).length ? errors : null;
